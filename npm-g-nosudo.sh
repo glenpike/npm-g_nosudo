c0a1a180c76ada113d9c30e61a3e0b4a6fde7dc3
Douglas Ross <doug@onvelocity.com>
2015-07-20 14:25:51 -0400

    https://github.com/glenpike/npm-g_nosudo/issues/11

--- npm-g-nosudo.sh
+++ npm-g-nosudo.sh
@@ -112,13 +112,13 @@ if [ -s $to_reinstall ]; then
 fi
 
 envfix='
-NPM_PACKAGES="%s"
-NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-PATH="$NPM_PACKAGES/bin:$PATH"
+export NPM_PACKAGES="%s"
+export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+export PATH="$NPM_PACKAGES/bin:$PATH"
 # Unset manpath so we can inherit from /etc/manpath via the `manpath`
 # command
 unset MANPATH  # delete if you already modified MANPATH elsewhere in your config
-MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
 '
 
 fix_env() {
